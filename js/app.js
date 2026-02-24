4e9547a688cb527f86b5bf47a9d0d3bc9fe16382
pascal <cal11611@gmail.com>
2026-02-24 09:42:02 +0100

    ajout banner image translation puis remplacement par vidéo

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
+// clouds
 const observer = new IntersectionObserver ((entries) => {
     for (const entry of entries){
-        console.log(
-            entry.target,
-            entry.isVisible
-            )
             if (entry.isIntersecting){
-                entry.target.classList.add('go')                        
+                entry.target.classList.add('go_nuages')                        
             }
             else{
-                entry.target.classList.remove('go')
+                entry.target.classList.remove('go_nuages')
             }
        }
     }
 )
 observer.observe(document.querySelector(".big_cloud"))
 observer.observe(document.querySelector(".little_cloud"))
+// banner
+const element = document.querySelector(".banner")
+const element2 = document.querySelector(".banner_video")
 
+
+window.onload = (event) => {
+    element.setAttribute("class"," banner go")
+    element2.setAttribute("class","banner_video banner_video_go")
+};
