d86bf5f260e114e0c752c723979ac1a92a6c99ce
pascal <cal11611@gmail.com>
2026-03-04 08:32:00 +0100

    template characters

---
+++ templates/characters.php
@@ -0,0 +1,35 @@
+    <h3>Les personnages</h3>
+    <div class="swiper-container swiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <figure>
+                <img src="<?php echo get_stylesheet_directory_uri() . '/images/Kawaneko.png'; ?>"/>
+                <figcaption>Kawaneko</figcaption>
+                </figure>
+            </div>
+            <div class="swiper-slide">
+                <figure>
+                <img src="<?php echo get_stylesheet_directory_uri() . '/images/Orenjiiro-1.png'; ?>"/>
+                <figcaption>Orenjiiro</figcaption>
+                </figure>
+            </div>
+            <div class="swiper-slide">
+                <figure>
+                <img src="<?php echo get_stylesheet_directory_uri() . '/images/Pinku-1.png'; ?>"/>
+                <figcaption>Pinku</figcaption>
+                </figure>
+            </div>
+            <div class="swiper-slide">
+                <figure>
+                <img src="<?php echo get_stylesheet_directory_uri() . '/images/Tenshi-1.png'; ?>"/>
+                <figcaption>Tenshi</figcaption>
+                </figure>
+            </div>
+            <div class="swiper-slide">
+                <figure>
+                <img src="<?php echo get_stylesheet_directory_uri() . '/images/Jaakuna-1.png'; ?>"/>
+                <figcaption>Jaakuna</figcaption>
+                </figure>
+            </div>           
+        </div>
+    </div>
