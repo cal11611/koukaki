3501b6c01051a534399b61d9ec243038ae0308ce
pascal <cal11611@gmail.com>
2026-03-10 19:22:40 +0100

    ajout menu burger

--- header.php
+++ header.php
@@ -33,19 +33,15 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'foce' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<nav id="site-navigation" class="main-navigation">
-            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-                <span class="line"></span>
-                <span class="line"></span>
-                <span class="line"></span>
-            </button>
-            <ul>
-                <li><a href="#story">Histoire</a></li>
-                <li><a href="#characters">Personnages</a></li>
-                <li class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
-                <li><a href="#place">Lieu</a></li>
-                <li><a href="#studio">Studio Koukaki</a></li>
-            </ul>
-
-		</nav><!-- #site-navigation -->
+		
+    <nav id="nav">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+        <ul>
+            <li><a href="#story">Histoire</a></li>
+            <li><a href="#myCharacters">Personnages</a></li>
+            <li><a href="#place">Lieu</a></li>
+            <li><a href="#studio">Studio Koukaki</a></li>
+         </ul>
+         <div id="icons"></div>
+  </nav>
 	</header><!-- #masthead -->
